cb9885b0a4773934f6d6836db72b942b4f9a6182
Gabriel Francisco <gf.bezerra10@gmail.com>
2026-08-17 16:22:07 -0300

    Add basic arithmetic and geometry operations

---
+++ ListaDeExercicios.py
@@ -0,0 +1,103 @@
+//Adição
+
+print ("Adição/Soma")
+
+a = int (input ("Insira um número: "))
+
+b = int (input ("Insira um número: "))
+
+soma = a + b
+
+print ("Soma = ", soma)
+
+//Subtração
+
+print ("Subtração")
+
+a = int (input ("Insira um número: "))
+
+b = int (input ("Insira um número: "))
+
+subtracao = a - b
+
+print ("Subtração = ", subtracao)
+
+//Multiplicação
+
+print ("Multiplicação")
+
+a = int (input ("Insira um número: "))
+
+b = int (input ("Insira um número: "))
+
+multiplicacao = a * b
+
+print ("Multiplicação = ", multiplicacao)
+
+//Divisão
+
+print ("Divisão")
+
+a = int (input ("Insira um número: "))
+
+b = int (input ("Insira um número: "))
+
+divisao = a / b
+
+print ("Divisão = ", divisao)
+
+//Dobro e Triplo
+
+print ("Dobro e Triplo")
+
+a = int(input ("Insira um número: "))
+
+dobro = a * 2
+
+print ("O dobro do número ",a, " é igual a: ", dobro)
+
+triplo = a * 3
+
+print ("O triplo do número ", a, " é igual a: ", triplo)
+
+//Sucessor e Antecessor
+
+print ("Sucessor e Antecessor")
+
+a = int(input("Insira um número: "))
+
+sucessor = a + 1
+
+print ("O sucessor do número ",a, " é igual a: ", sucessor)
+
+antecessor = a - 1
+
+print ("O antecessor do número ",a, " é igual a: ", antecessor)
+
+//Área do Triângulo
+
+print ("Área do Triângulo")
+
+a = int(input("Digite a medida da base do triângulo: "))
+
+b = int(input("Digite a medida da altura do triãngulo: "))
+
+area = (a * b)/2
+
+print("A área desse triângulo é de: ", area)
+
+//Perímetro do Retângulo
+
+print ("Perímetro do Retângulo")
+
+a = int(input("Digite a medida do lado 1 do retângulo: "))
+
+b = int(input("Digite a medida do lado 2 do retângulo: "))
+
+c = int(input("Digite a medida do lado 3 do retângulo: "))
+
+d = int(input("Digite a medida do lado 4 do retângulo: "))
+
+perimetro = a + b + c + d
+
+print ("O perímetro do retângulo é igual a: ", perimetro)
